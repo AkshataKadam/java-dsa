2c9fbde2b29bf8f9958aa291c8acdd565c9e97c1
AkshataKadam <akshatakadam1908@gmail.com>
2025-08-28 19:25:21 +0530

    adds multi-threading concepts

--- src/multithreading/MultiThreadingConcepts.java
+++ src/multithreading/MultiThreadingConcepts.java
@@ -21,8 +21,8 @@ public class MultiThreadingConcepts {
             }
         });
 
-        Thread t2 = new Thread(() -> obj.task2());
-        Thread t3 = new Thread(() -> obj.task3());
+        Thread t2 = new Thread(obj::task2);
+        Thread t3 = new Thread(obj::task3);
 
         t1.start();
         t2.start();
